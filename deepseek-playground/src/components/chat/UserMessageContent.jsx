a8c74e0bcf22aee7c53d63baf303ba2c10c81e2b
fabiojbg <fabiojbg@users.noreply.github.com>
2025-01-27 12:00:39 -0300

    Removing and ignoring the transpiled .jsx files

--- deepseek-playground/src/components/chat/UserMessageContent.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export default function UserMessageContent(_a) {
-    var content = _a.content;
-    return <div className="whitespace-pre-line">{content}</div>;
-}
--- deepseek-playground/src/components/misc/Github.jsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link";
-import React from "react";
-import { BsGithub } from "react-icons/bs";
-export default function Github(_a) {
-    return (<Link className="flex flex-col items-center border-white/10 py-2 text-primary/80 transition-colors hover:text-primary" href="https://github.com/Nashex/gpt4-playground">
-      <BsGithub className="text-3xl"/>
-    </Link>);
-}
